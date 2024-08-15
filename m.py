26d486a9efd7b1d5b2644c201b214a1ac3d233dc
MarceloSmg <celomsg@hotmail.com>
2024-08-14 21:42:10 -0300

    Adicionar arquivos m.py e a.py

---
+++ m.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Configuração da página
+st.set_page_config(page_title="Instituto Tarumã Pesquisa", page_icon="🌲")
+
+# Título da página
+st.title("🌲 Instituto Tarumã Pesquisa")
